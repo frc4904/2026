909c2a2d6384f4821d23fe70bb9c7b71cc35d68e
Sam Matteson <sammatt@nuevaschool.org>
2026-03-04 21:59:03 -0800

    terrible motor speed modifier code

--- src/main/java/org/usfirst/frc4904/standard/custom/motorcontrollers/SmartMotorController.java
+++ src/main/java/org/usfirst/frc4904/standard/custom/motorcontrollers/SmartMotorController.java
@@ -12,4 +12,61 @@ public interface SmartMotorController extends MotorController {
 	boolean isRevLimitSwitchPressed() throws IllegalAccessException;
 
 	void setMotorBrake(boolean brake);
+
+	default SmartMotorController withSpeedMultiplier(double multiplier) {
+		return new SpeedModifiedSmartMotor(this, multiplier);
+	}
+
+	class SpeedModifiedSmartMotor extends SmartMotorWrapper {
+		private final double multiplier;
+
+		public SpeedModifiedSmartMotor(SmartMotorController motor, double multiplier) {
+			super(motor);
+			this.multiplier = multiplier;
+		}
+
+		@Override
+		public SmartMotorController withSpeedMultiplier(double multiplier) {
+			return new SpeedModifiedSmartMotor(motor, multiplier * this.multiplier);
+		}
+
+		@Override
+		public void set(double speed) {
+			super.set(speed * multiplier);
+		}
+
+		@Override
+		public double get() {
+			return super.get() / multiplier;
+		}
+	}
+}
+
+abstract class SmartMotorWrapper implements SmartMotorController {
+	protected final SmartMotorController motor;
+
+	protected SmartMotorWrapper(SmartMotorController motor) {
+		this.motor = motor;
+	}
+
+	@Override
+	public boolean isFwdLimitSwitchPressed() throws IllegalAccessException {
+		return motor.isFwdLimitSwitchPressed();
+	}
+	@Override
+	public boolean isRevLimitSwitchPressed() throws IllegalAccessException {
+		return motor.isRevLimitSwitchPressed();
+	}
+
+	@Override public void setMotorBrake(boolean brake) { motor.setMotorBrake(brake); }
+
+	@Override public void set(double speed) { motor.set(speed); }
+	@Override public double get() { return motor.get(); }
+
+	@Override public void setInverted(boolean isInverted) { motor.setInverted(isInverted); }
+	@Override public boolean getInverted() { return motor.getInverted(); }
+
+	@Override public void disable() { motor.disable(); }
+
+	@Override public void stopMotor() { motor.stopMotor(); }
 }
