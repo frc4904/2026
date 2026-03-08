e2471320e24555b27955cbb648988f818dcf7e93
Robby20914 <sofia.robison@gmail.com>
2026-03-08 11:44:27 -0700

    aeFdfbsghdfhgj,

--- src/main/java/org/usfirst/frc4904/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/org/usfirst/frc4904/robot/subsystems/ShooterSubsystem.java
@@ -51,3 +51,55 @@ public class ShooterSubsystem extends MotorSubsystem {
     }
 
 }
+/*package org.usfirst.frc4904.robot.subsystems;
+
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import org.usfirst.frc4904.standard.custom.motorcontrollers.CustomTalonFX;
+import org.usfirst.frc4904.standard.custom.motorcontrollers.SmartMotorController;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+import java.util.function.DoubleSupplier;
+
+public class ShooterSubsystem extends MotorSubsystem {
+
+    private static final Slot0Configs pidfConfig = new Slot0Configs();
+    static {
+        // TODO tune
+        pidfConfig.kP = 1;
+        pidfConfig.kI = 0;
+        pidfConfig.kD = 0;
+        pidfConfig.kS = 0;
+        pidfConfig.kV = 0;
+        pidfConfig.kA = 0;
+        pidfConfig.kG = 0;
+    }
+
+    public ShooterSubsystem(CustomTalonFX shooterMotor1, CustomTalonFX shooterMotor2) {
+        super(
+            new SmartMotorController[] { shooterMotor1, shooterMotor2 },
+            4
+        );
+
+        shooterMotor1.getConfigurator().apply(pidfConfig);
+        shooterMotor2.getConfigurator().apply(pidfConfig);
+    }
+
+    public Command c_basicShoot() {
+        return c_forward(true);
+    }
+
+    private final VelocityVoltage velocityRequest = new VelocityVoltage(0);
+
+    public Command c_controlVelocity(DoubleSupplier getVelocity) {
+        return runEnd(() -> {
+            velocityRequest.withVelocity(getVelocity.getAsDouble());
+            for (var motor : motors) {
+                ((CustomTalonFX) motor).setControl(velocityRequest);
+            }
+        }, this::stop);
+    }
+
+}
+ */
